5289dfd2e8f6121a3dca91321c6b7539daebb750
heiheiheiheiheihei <897891109@qq.com>
2019-08-13 13:21:18 +0800

    uuu

--- FindTarget.py
+++ FindTarget.py
@@ -15,20 +15,17 @@ def findLight():
     # 寻找连通矩形
     img2, contours, hierarchy = cv2.findContours(img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-    # 获取最小包围矩形
-    rect = cv2.minAreaRect(contours[n])
-    n = n + 1
-    # 中心坐标
-    x, y = rect[0]
-    cv2.circle(img, (int(x), int(y)), 1, (0, 0, 255), 5)
-
-    # 长宽,总有 width>=height
-    width, height = rect[1]
-
-    # 角度:[-90,0)
-    angle = rect[2]
-
-    cv2.drawContours(img, contour, -1, (255, 255, 0), 3)
-    #cv2.putText()
-    print('width=', width, 'height=', height,
+        # 获取最小包围矩形
+        rect = cv2.minAreaRect(contours[n])
+        n = n + 1
+        # 中心坐标
+        x, y = rect[0]
+        cv2.circle(img, (int(x), int(y)), 1, (0, 0, 255), 5)
+        # 长宽,总有 width>=height
+        width, height = rect[1]
+        # 角度:[-90,0)
+        angle = rect[2]
+        cv2.drawContours(img, contour, -1, (255, 255, 0), 3)
+        #cv2.putText()
+        print('width=', width, 'height=', height,
           'x=', x, 'y=', y, 'angle=', angle, '$$$=', str(width / height))
