38fca07dc6be4127f3f2dd5f4dee7f3ad977b782
heiheiheiheiheihei <897891109@qq.com>
2019-09-16 23:40:26 +0800

    update

--- C/main.c
+++ C/main.c
@@ -1,23 +1,42 @@
 #include<stdio.h>
-unsigned long Fact(unsigned int n);
+#define STUD_N    40
+#define  COURSE_N  3
+void AverforStd(int score[][COURSE_N],int sum[],float avar[],int n);
+void AverforStd(int score[][COURSE_N],int sum[],float avar[],int n);
+int main()
+{int score[STUD_N][COURSE_N],sumS[STUD_N],sumC[COURSE_N],n;    
+long m=num[STUD_N]
+float averS[STUD_N],averC[COURSE_N]
+printf("input the total number of the students(n<=40):")
+scanf("%d,&n")
+ReadScore(score,num,n)
+{
+    int i,j;
+	for(i=0;i<n;i++)
+	{
+		sum[i]=0;
+		for(j=0;j<COURSE_N;j++)
+		{
+			sum[i]+sum[i]+score[i][j];
+		}
+		avar[i]=(float)sum[i]/COURSE_N;
+	}
+}
 int main()
 {
-	int m,k;
-	unsigned long p;
-	do{
-		printf("Input m,k (m>=k>0):");
-		scanf("%d,%d",&m,&k);
-	}while(m<k||m<=0||k<0);
-	p=Fact(m)/(Fact(k)*Fact(m-k));
-	printf("p=%lu\n",p);
-	scanf("%d",&m);
+	float x,amax,amin;
+	scanf("%f",&x);
+	amax=x;
+	amin=x;
+	while(x>=0)
+	{
+		if(x>amax)
+		amax=x;
+		if(x<amin)
+		amin=x;
+		scanf("%f",&x);
+	}
+	printf("\namax=%f\namin=%f\n",amax,amin);
+	scanf("%f",&x);
 	return 0;
-}
-unsigned long Fact(unsigned int n)
-{
-	unsigned int i;
-    unsigned long result=1;
-	for(i=2;i<=n;i++)
-		result *=i;
-	return result;
 }
